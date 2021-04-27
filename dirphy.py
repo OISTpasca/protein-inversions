4a1fae3f1645005ef9bb34ad52828bbb44ce1a13
stefanopascarelli <pasca1989@hotmail.it>
2021-04-27 16:01:51 +0900

    Added pseudo counts modifications to aa frequencies

--- dirphy.py
+++ dirphy.py
@@ -2,6 +2,7 @@ import sys
 import dendropy
 from Bio import SeqIO
 from Bio.SubsMat import MatrixInfo
+import numpy as np
 import os
 import argparse
 import pandas as pd
@@ -28,6 +29,7 @@ def create_parser(argv):
     parser.add_argument('--min-coverage', help='if more gap frequency than this parameter (0 to 1) then swapping score is 0', default=0,
                         type=float)
     parser.add_argument('--max-swapset', help='max number of elements in swap set', default=999, type=int)
+    parser.add_argument('--pseudocounts', help='whether using the pseudocounts correction of the aa freq.', default=True)
 
     parser.add_argument('--cons-score-function', help='identity: percentage identity (0-1); blosum: blosum score (0-1)', default='blosum')
 
@@ -53,10 +55,10 @@ class Dirphy:
         init the stuff needed for analysis
         :param args:
         """
+        # params
         self.MAX_DISTANCE = args.max_distance
         self.MIN_DIFF = args.min_score_diff
         self.MAX_POS = args.max_codon_pos
-
         self.MAX_SWAPSET = args.max_swapset
 
         self.MIN_COVER = args.min_coverage
@@ -66,6 +68,8 @@ class Dirphy:
         }
         self.CONS_SCORE_FUNC = score_funcs[args.cons_score_function]
         self.MULTI_PROT_APPROACH = args.multi_protein_approach
+        self.PSEUDOCOUNTS = args.pseudocounts
+        # input
         self.tree = self.get_tree(args)
         self.dm = self.get_distance_matrix(args)
         self.ds = self.get_sequence_dict(args)
@@ -95,6 +99,11 @@ class Dirphy:
 
         # utilities
         self.AAlist = list('ACDEFGHIKLMNPQRSTVWY')
+
+        # pseudocounts bg and pij
+        self.beta = 5
+        self.BGprob = self.get_aa_background()
+        self.qij_mat, self.qij_sum = self.get_qij_matrix()
 
     def get_sequence_dict(self, args):
         """
@@ -234,6 +243,63 @@ class Dirphy:
             if name in self.orthologs[prot]:
                 protein = prot
         return protein
+
+    # function that stores amino acid bg
+    def get_aa_background(self):
+        """
+        stores aa background in an array of len 20 with self.AAlist as labels
+        now from Le S.Q., Gascuel O. Molecular Biology and Evolution. 2008 25(7):1307-20.
+        :return:
+        """
+        # BLOSUM62 background distribution
+        blosum_background_distr = [0.079066, 0.055941, 0.041977, 0.053052, 0.012937, 0.040767, 0.071586, 0.057337,
+                                   0.022355, 0.062157, 0.099081, 0.064600, 0.022951, 0.042302, 0.044040, 0.061197,
+                                   0.053287, 0.012066, 0.034155, 0.069147]
+        amino_acids = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V']
+        capra_bg = {amino_acids[i]:blosum_background_distr[i] for i in range(20)}
+        reordered_bg = [capra_bg[AA] for AA in self.AAlist]
+        return reordered_bg
+
+    # function to get the qij matrix
+    def get_qij_matrix(self):
+        """
+        qij is the matrix of the frequencies of co occurrence of two amino acids as calculated by the blosum formula
+        qij = Pi * Pj * e^(lambda*BLOSij)
+        :return:
+        """
+
+        order = [self.AAlist.index(x) for x in "A R N D C Q E G H I L K M F P S T W Y V".split()]
+        O = lambda x: order[x]
+        qi_raw = """
+0.0215 
+0.0023 0.0178 
+0.0019 0.0020 0.0141 
+0.0022 0.0016 0.0037 0.0213 
+0.0016 0.0004 0.0004 0.0004 0.0119 
+0.0019 0.0025 0.0015 0.0016 0.0003 0.0073 
+0.0030 0.0027 0.0022 0.0049 0.0004 0.0035 0.0161 
+0.0058 0.0017 0.0029 0.0025 0.0008 0.0014 0.0019 0.0378 
+0.0011 0.0012 0.0014 0.0010 0.0002 0.0010 0.0014 0.0010 0.0093 
+0.0032 0.0012 0.0010 0.0012 0.0011 0.0009 0.0012 0.0014 0.0006 0.0184 
+0.0044 0.0024 0.0014 0.0015 0.0016 0.0016 0.0020 0.0021 0.0010 0.0114 0.0371 
+0.0033 0.0062 0.0024 0.0024 0.0005 0.0031 0.0041 0.0025 0.0012 0.0016 0.0025 0.0161 
+0.0013 0.0008 0.0005 0.0005 0.0004 0.0007 0.0007 0.0007 0.0004 0.0025 0.0049 0.0009 0.0040 
+0.0016 0.0009 0.0008 0.0008 0.0005 0.0005 0.0009 0.0012 0.0008 0.0030 0.0054 0.0009 0.0012 0.0183 
+0.0022 0.0010 0.0009 0.0012 0.0004 0.0008 0.0014 0.0014 0.0005 0.0010 0.0014 0.0016 0.0004 0.0005 0.0191 
+0.0063 0.0023 0.0031 0.0028 0.0010 0.0019 0.0030 0.0038 0.0011 0.0017 0.0024 0.0031 0.0009 0.0012 0.0017 0.0126 
+0.0037 0.0018 0.0022 0.0019 0.0009 0.0014 0.0020 0.0022 0.0007 0.0027 0.0033 0.0023 0.0010 0.0012 0.0014 0.0047 0.0125 
+0.0004 0.0003 0.0002 0.0002 0.0001 0.0002 0.0003 0.0004 0.0002 0.0004 0.0007 0.0003 0.0002 0.0008 0.0001 0.0003 0.0003 0.0065 
+0.0013 0.0009 0.0007 0.0006 0.0003 0.0007 0.0009 0.0008 0.0015 0.0014 0.0022 0.0010 0.0006 0.0042 0.0005 0.0010 0.0009 0.0009 0.0102 
+0.0051 0.0016 0.0012 0.0013 0.0014 0.0012 0.0017 0.0018 0.0006 0.0120 0.0095 0.0019 0.0023 0.0026 0.0012 0.0024 0.0036 0.0004 0.0015 0.0196 
+""".split()
+        qij_mat = [[0 for i in range(20)] for j in range(20) ]
+        counter = 0
+        for i in range(20):
+            for j in range(i+1):
+                qij_mat[O(i)][O(j)] = qij_mat[O(j)][O(i)] = float(qi_raw[counter])
+                counter += 1
+        return qij_mat, [sum([x[i] for x in qij_mat]) for i in range(20)]
+
 
     def run_analysis(self):
         """
@@ -414,10 +480,10 @@ class Dirphy:
         meta_score = 0
         ss_score = 0
 
-        a  = self.frequency_array(this_prot_str)
-        d  = self.frequency_array(other_prot_str)
-        b  = self.frequency_array(this_prot_swap_str)
-        c  = self.frequency_array(other_prot_swap_str)
+        a = self.frequency_array(this_prot_str)
+        d = self.frequency_array(other_prot_str)
+        b = self.frequency_array(this_prot_swap_str)
+        c = self.frequency_array(other_prot_swap_str)
         assert all([len(x) == 20 for x in [a, b, c, d]]), "WTF the length is not 20?!"
 
         # matching probabilities
@@ -448,12 +514,31 @@ class Dirphy:
     def frequency_array(self, array):
         """
         return the array in single letter amino acid order of the frequency of occurrences given the list
+        PSEUDOCOUNTS, when active, uses the LG matrix of aa transition and the formula of better scoring schemes..
+        X AND gaps are removed!
         :param array:
         :return:
         """
         count = Counter(array)
+        del(count['-'])
+        del(count['X'])
         sum_arr = sum(count.values())
-        return [count.get(x, 0)/sum_arr for x in self.AAlist]
+        freq_arr = [count.get(x, 0)/sum_arr for x in self.AAlist]
+        assert round(sum(freq_arr),3) == 1, "the sum of all frequencies is not 1, {}".format(count)
+        alpha = sum_arr
+        beta = self.beta
+        if self.PSEUDOCOUNTS:
+            new_freq_arr = []
+            for a in range(len(self.AAlist)):
+                new_freq = freq_arr[a] * alpha / (alpha + beta)
+                pseudo_sum = 0
+                for i in range(len(self.AAlist)):
+                    pseudo_sum += freq_arr[i] * self.qij_mat[i][a] / self.qij_sum[i]
+                pseudo_sum *= beta
+                new_freq += (pseudo_sum / (alpha + beta))
+                new_freq_arr.append(new_freq)
+            freq_arr = new_freq_arr
+        return freq_arr
 
     def save_output(self):
         """
